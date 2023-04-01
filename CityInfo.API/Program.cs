612bd5400a999945bfdbec8cb5ac7b8364fb698d
Sandip Punalkar <sandippunalkar9@gmail.com>
2023-04-01 09:55:15 +0530

    Partially Updating a Resource

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(option =>
 {
     option.ReturnHttpNotAcceptable=true;
-}).AddXmlDataContractSerializerFormatters();
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
