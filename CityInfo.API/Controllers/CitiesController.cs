edf2d226d687483470d101af26cba41139396010
Sandip Punalkar <sandippunalkar9@gmail.com>
2023-04-01 18:40:01 +0530

    Returning Data from the Repository When Requesting Resources

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,5 +1,6 @@
 ﻿using CityInfo.API.Data;
 using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,29 +10,42 @@ namespace CityInfo.API.Controllers
     [ApiController]
     public class CitiesController : ControllerBase
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        private readonly ICityInfoRepository _cityInfoRepository;
 
-        public CitiesController(CitiesDataStore citiesDataStore)
+        public CitiesController(ICityInfoRepository cityInfoRepository)
         {
-            this._citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            this._cityInfoRepository = cityInfoRepository ?? throw new ArgumentNullException(nameof(cityInfoRepository));
         }
+
         [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointsOfInterestDto>>> GetCities()
         {
-            return Ok(_citiesDataStore.Cities);
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
-        {
-            var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
-
-            if (cityToReturn == null)
+            var cityEntities = await _cityInfoRepository.GetCitiesAsync();
+            var result = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var cityEntity in cityEntities)
             {
-                return NotFound();
+                result.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Description = cityEntity.Description,
+                    Name = cityEntity.Name,
+                });
             }
 
-            return Ok(cityToReturn);
+            return Ok(result);
         }
+
+        //[HttpGet("{id}")]
+        //public ActionResult<CityDto> GetCity(int id)
+        //{
+        //    var cityToReturn = _citiesDataStore.Cities.FirstOrDefault(c => c.Id == id);
+
+        //    if (cityToReturn == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(cityToReturn);
+        //}
     }
 }
