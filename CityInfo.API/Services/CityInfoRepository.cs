b36e2fc90482084c7a21047dbc7b4fec6efa2de9
Sandip Punalkar <sandippunalkar9@gmail.com>
2023-04-02 08:44:54 +0530

    Returning Pagination Metadata

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -18,13 +18,15 @@ namespace CityInfo.API.Services
             return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
         }
 
-        public async Task<IEnumerable<City>> GetCitiesAsync(string? name, string? searchQuery, int pageNumber, int pageSize)
+        public async Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(
+            string? name, string? searchQuery, int pageNumber, int pageSize)
         {
+            // collection to start from
             var collection = _context.Cities as IQueryable<City>;
 
             if (!string.IsNullOrWhiteSpace(name))
             {
-                searchQuery = name.Trim();
+                name = name.Trim();
                 collection = collection.Where(c => c.Name == name);
             }
 
@@ -35,11 +37,18 @@ namespace CityInfo.API.Services
                     || (a.Description != null && a.Description.Contains(searchQuery)));
             }
 
-            return await collection.OrderBy(c => c.Name)
+            var totalItemCount = await collection.CountAsync();
+
+            var paginationMetadata = new PaginationMetadata(totalItemCount, pageSize, pageNumber);
+
+            var collectionToReturn = await collection.OrderBy(c => c.Name)
                 .Skip(pageSize * (pageNumber - 1))
                 .Take(pageSize)
                 .ToListAsync();
+
+            return (collectionToReturn, paginationMetadata);
         }
+
 
         public async Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest)
         {
